e14d59b584033f863e8c21a4e8302d52f1ecc43e
bruno_teider <107257148+bruno-teider@users.noreply.github.com>
2024-07-26 12:47:30 -0300

    Changed visuals, now pressing 'Space' generates another palette, pressing 'c' will copy the palette to clipboard

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Snackbar from "@mui/material/Snackbar";
+import useKeyPress from "./hooks/usekeyPress";
+import rgbHex from "rgb-hex";
 import "./index.css";
 
 function App() {
-  const [palette, setPalette] = useState([])
+  const [palette, setPalette] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  function CopyToClipboard(text) { 
+    if (document.hasFocus()) {
+      navigator.clipboard.writeText(text).then(() => {
+        setOpen(true);
+      });
+    } else {
+      console.warn("Document is not focused, unable to copy to Clipboard");
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false)
+  };
 
   const getPalette = async () => {
-    const apiUrl = '/api/'
+    const apiUrl = "/api/";
     const apiModel = {
-      "model": "default"
-    }
+      model: "default",
+    };
 
     try {
       const response = await axios.post(apiUrl, apiModel, {
         headers: {
-            'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-    });
-      setPalette(response.data.result)
+      })
+      setPalette(response.data.result.map((color, index) =>
+        rgbHex(color.join(","))
+      ))
     } catch (error) {
       console.log("Error: ", error)
     }
-  }
+  };
 
   useEffect(() => {
     getPalette()
   }, [])
 
+  useKeyPress(" ", getPalette)
+
+  useKeyPress("c", () => CopyToClipboard(palette))
+
   return (
     <div className="App">
 
+      {open && (
+        <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+      />
+      )}
+
       <div className="container">
         {palette.map((color, index) => (
-          <div key={index} style={{ backgroundColor: `rgb(${color.join(",")})`, width: 50, height: 50 }}/>
+          <div key={index} style={{ backgroundColor: `#${color}`, width: 150, height: 225 }}/>
         ))}
       </div>
 
-      <button onClick={getPalette}>Generate Random Palette</button>      
+      <button onClick={getPalette} className="button">Generate Random Palette</button>
     </div>
   );
 }
---
+++ src/hooks/usekeyPress.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+export default function useKeyPress(key, action) {
+    useEffect(() => {
+        function onKeyup(e) {
+            if (e.key === key) action()
+        }
+        window.addEventListener('keyup', onKeyup)
+
+        return () => [
+            window.removeEventListener('keyup', onKeyup)
+        ]
+    }, [key, action])
+}
